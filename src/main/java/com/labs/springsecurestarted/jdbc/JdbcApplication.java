46c051ee938b3e3ae62474b5fcb3d465e63a4ac5
Luis.BolanosS Luis.BolanosS@perficient.com <Luis.BolanosS@perficient.com>
2020-08-01 18:37:14 -0500

    Create JDBC Spring secure practice

---
+++ src/main/java/com/labs/springsecurestarted/jdbc/JdbcApplication.java
@@ -0,0 +1,78 @@
+package com.labs.springsecurestarted.jdbc;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.security.Principal;
+
+@SpringBootApplication
+public class JdbcApplication {
+	@Bean
+	UserDetailsManager userDetailsManager(DataSource dataSource) {
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+		jdbcUserDetailsManager.setDataSource(dataSource);
+
+		return jdbcUserDetailsManager;
+	}
+
+	@Bean
+	InitializingBean initializer(UserDetailsManager userDetailsManager) {
+		return () -> {
+			String joshUsername = "josh";
+
+			if (!userDetailsManager.userExists(joshUsername)) {
+				UserDetails josh = User.withDefaultPasswordEncoder()
+						.username(joshUsername)
+						.password("password")
+						.roles("USER")
+						.build();
+
+				userDetailsManager.createUser(josh);
+			}
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcApplication.class, args);
+	}
+}
+
+@RestController
+class GreetingRestController {
+	@GetMapping("/greeting")
+	String greeting(Principal principal) {
+		return "hello, " + principal.getName();
+	}
+}
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+
+		http.authorizeRequests()
+			.antMatchers("/", "/h2-console/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+		.formLogin()
+			.loginPage("/login")
+			.permitAll()
+			.and()
+		.logout().permitAll();
+	}
+}
+
---
+++ src/test/java/com/labs/springsecurestarted/SpringSecureStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labs.springsecurestarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecureStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

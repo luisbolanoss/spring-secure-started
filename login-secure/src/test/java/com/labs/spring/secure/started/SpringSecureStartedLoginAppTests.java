b02184ee361cbb1105b4f04b731a7e0555a69639
Luis.BolanosS Luis.BolanosS@perficient.com <Luis.BolanosS@perficient.com>
2020-08-02 14:17:19 -0500

    Login Spring secure practice to a module

---
+++ login-secure/src/test/java/com/labs/spring/secure/started/SpringSecureStartedLoginAppTests.java
@@ -0,0 +1,13 @@
+package com.labs.spring.secure.started;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecureStartedLoginAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
